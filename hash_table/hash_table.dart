b8664f142ddcc410f06e2202e1d37f4949f8ec13
Safwan Pulisseri <safwanpulisseri123@gmail.com>
2024-02-20 14:04:58 +0530

    hash table two versions

---
+++ hash_table/hash_table.dart
@@ -0,0 +1,57 @@
+class Node {
+  String key;
+  dynamic data;
+  Node? next;
+
+  Node(this.key, this.data);
+}
+
+class HashTable {
+  List<Node?>? table;
+  int size;
+
+  HashTable(this.size) {
+    table = List.generate(size, (index) => null);
+  }
+
+  int _getIndex(String key) {
+    return key.hashCode.abs() % size;
+  }
+
+  void add(String key, dynamic data) {
+    int index = _getIndex(key);
+    Node newNode = Node(key, data);
+    if (table?[index] == null) {
+      table?[index] = newNode;
+    } else {
+      newNode.next = table?[index];
+      table?[index] = newNode;
+    }
+  }
+
+  void getAll() {
+    for (int i = 0; i < size; i++) {
+      if (table?[i] != null) {
+        Node? temp = table?[i];
+        print('At index $i:');
+        while (temp != null) {
+          print('${temp.data}');
+          temp = temp.next;
+        }
+      } else {
+        print('At index $i, there is no value found.');
+      }
+    }
+  }
+}
+
+void main() {
+  HashTable hashTable = HashTable(5);
+  hashTable.add('sugith', 24);
+  hashTable.add('rocky', 29);
+  hashTable.add('ram', 21);
+
+  hashTable.getAll();
+
+  hashTable.getAll();
+}
