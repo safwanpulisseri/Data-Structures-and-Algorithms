40f676cbf329836976060eecc3a29b5744cc6449
Safwan Pulisseri <safwanpulisseri123@gmail.com>
2024-02-22 14:26:02 +0530

    queue in linked list

--- Queue/queue_in_linked.dart
+++ Queue/queue_in_linked.dart
@@ -1,1 +1,47 @@
+class Node {
+  int data;
+  Node? next;
+  Node(this.data);
+}
 
+class Queue {
+  Node? front;
+  Node? rear;
+  enQueue(int data) {
+    Node newQueue = Node(data);
+    if (front == null) {
+      front = newQueue;
+      rear = newQueue;
+    } else {
+      rear?.next = newQueue;
+    }
+    rear = newQueue;
+  }
+
+  deQueue() {
+    if (front != null) {
+      front = front?.next;
+//       if (front == null) {
+//         rear = null;
+//       }
+    }
+  }
+
+  display() {
+    Node? temp = front;
+    while (temp != null) {
+      print(temp.data);
+      temp = temp.next;
+    }
+  }
+}
+
+void main() {
+  Queue queue = Queue();
+  queue.enQueue(10);
+  queue.enQueue(20);
+  queue.enQueue(30);
+  queue.enQueue(40);
+  queue.deQueue();
+  queue.display();
+}
